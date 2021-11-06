1e7a7621394aca76ab778befbc97d65550202817
dgiovannelli <donato.giovannelli@gmail.com>
2021-11-06 17:52:56 +0100

    updated teaching and research page

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -326,10 +326,10 @@ if (typeof jQuery === 'undefined') {
 
   Carousel.VERSION  = '3.3.6'
 
-  Carousel.TRANSITION_DURATION = 600
+  Carousel.TRANSITION_DURATION = 300
 
   Carousel.DEFAULTS = {
-    interval: 5000,
+    interval: 3000,
     pause: 'hover',
     wrap: true,
     keyboard: true
